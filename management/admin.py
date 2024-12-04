457428670589b44d14318ff99eb8ce15b5f604ad
Tahsinul Muntasir Moon <muntasirmarshmallow@gmail.com>
2024-12-04 15:40:55 +0600

    template and media

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (GarbageCategory,Garbage,Cleaner,GarbageOrder)
 
 # Register your models here.
+class GarbageAdmin(admin.ModelAdmin):
+
+    list_display = ['__str__', 'slug','status','weight',]
+
+    class Meta:
+        model=Garbage
+
+class CleanerAdmin(admin.ModelAdmin):
+
+    list_display = ['__str__', 'name','cleaning_task','price','contact']
+
+    class Meta:
+        model=Cleaner
+
+
+admin.site.register(Cleaner,CleanerAdmin)
+admin.site.register(Garbage,GarbageAdmin)
+admin.site.register(GarbageCategory)
+admin.site.register(GarbageOrder)
